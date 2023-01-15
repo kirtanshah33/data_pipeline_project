5918a429ccc31e8b1c9e82bfd4fb8314b25b2439
kirtanshah33 <kirtanshah33@gmail.com>
2023-01-15 20:27:01 +0530

    insert csv to delta table

--- python_scripts/extract_mysql_incremental.py
+++ python_scripts/extract_mysql_incremental.py
@@ -12,50 +12,48 @@ spark = configure_spark_with_delta_pip(builder).getOrCreate()
 
 last_record_sql = """SELECT COALESCE(MAX(LastUpdated), '1900-01-01')
     FROM delta.`/opt/application/delta_tables/orders`;"""
-result = spark.sql(last_record_sql).first()[0]
-print(result)
+result = spark.sql(last_record_sql).first()
 # # there's only one row and column returned
-# last_updated_warehouse = result[0]
+last_updated_warehouse = result[0]
 
-# rs_cursor.close()
-# rs_conn.commit()
 
-# # get the MySQL connection info and connect
-# parser = configparser.ConfigParser()
-# parser.read("pipeline.conf")
-# hostname = parser.get("mysql_config", "hostname")
-# port = parser.get("mysql_config", "port")
-# username = parser.get("mysql_config", "username")
-# dbname = parser.get("mysql_config", "database")
-# password = parser.get("mysql_config", "password")
+# get the MySQL connection info and connect
+parser = configparser.ConfigParser()
+parser.read("pipeline.conf")
+hostname = parser.get("mysql_config", "hostname")
+port = parser.get("mysql_config", "port")
+username = parser.get("mysql_config", "username")
+dbname = parser.get("mysql_config", "database")
+password = parser.get("mysql_config", "password")
 
-# conn = pymysql.connect(host=hostname,
-#         user=username,
-#         password=password,
-#         db=dbname,
-#         port=int(port))
+conn = pymysql.connect(host=hostname,
+        user=username,
+        password=password,
+        db=dbname,
+        port=int(port))
 
-# if conn is None:
-#   print("Error connecting to the MySQL database")
-# else:
-#   print("MySQL connection established!")
+if conn is None:
+  print("Error connecting to the MySQL database")
+else:
+  print("MySQL connection established!")
 
-# m_query = """SELECT *
-#     FROM Orders
-#     WHERE LastUpdated > %s;"""
-# local_filename = "order_extract.csv"
+m_query = """SELECT *
+    FROM Orders
+    WHERE LastUpdated > %s;"""
+local_filename = "order_extract.csv"
 
-# m_cursor = conn.cursor()
-# m_cursor.execute(m_query, (last_updated_warehouse,))
-# results = m_cursor.fetchall()
+m_cursor = conn.cursor()
+m_cursor.execute(m_query, (last_updated_warehouse,))
+results = m_cursor.fetchall()
 
-# with open(local_filename, 'w') as fp:
-#   csv_w = csv.writer(fp, delimiter='|')
-#   csv_w.writerows(results)
+with open(local_filename, 'w') as fp:
+  csv_w = csv.writer(fp, delimiter='|')
+  csv_w.writerows(results)
 
-# fp.close()
-# m_cursor.close()
-# conn.close()
+fp.close()
+m_cursor.close()
+conn.close()
+
 
 # # load the aws_boto_credentials values
 # parser = configparser.ConfigParser()
